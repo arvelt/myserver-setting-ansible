e0eba415d6a98d2189dc4d6aeda0ac0354081369
Keisuke Ota <arvelt.s@gmail.com>
2014-10-07 03:31:53 +0900

    add mysql and ownclound

--- runansuble.sh
+++ runansuble.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
 source ~/Dropbox/backup/mytodo-sinatra/config.ini
+source ~/Dropbox/backup/owncloud/config.ini
 vars="{\"TODOSINATRA_CLIENT_ID\":\"$TODOSINATRA_CLIENT_ID\",
-        \"TODOSINATRA_CLIENTSECRET_ID\":\"$TODOSINATRA_CLIENTSECRET_ID\"}"
+        \"TODOSINATRA_CLIENTSECRET_ID\":\"$TODOSINATRA_CLIENTSECRET_ID\",
+        \"OWNCLOUD_DB\":\"$OWNCLOUD_DB\",
+        \"OWNCLOUD_USER\":\"$OWNCLOUD_USER\",
+        \"OWNCLOUD_PASSWORD\":\"$OWNCLOUD_PASSWORD\",
+        \"MYSQL_ROOT_PASSWORD\":\"$MYSQL_ROOT_PASSWORD\"}"
 ansible-playbook -i hosts  --extra-vars="${vars}" site.yml
